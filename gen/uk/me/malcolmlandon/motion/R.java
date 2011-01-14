2507909f8594f0aa49c1dff21b7dfd7df90dd690
Malcolm Landon <malcolm@landonsonline.me.uk>
2011-01-14 21:03:45 +0000

    Added pressed images.

--- gen/uk/me/malcolmlandon/motion/R.java
+++ gen/uk/me/malcolmlandon/motion/R.java
@@ -13,11 +13,19 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int pause=0x7f020001;
-        public static final int snapshot=0x7f020002;
-        public static final int start=0x7f020003;
-        public static final int status=0x7f020004;
-        public static final int widget_bg_normal=0x7f020005;
-        public static final int widget_frame_portrait=0x7f020006;
+        public static final int pause_pressed=0x7f020002;
+        public static final int pause_selector=0x7f020003;
+        public static final int snapshot=0x7f020004;
+        public static final int snapshot_pressed=0x7f020005;
+        public static final int snapshot_selector=0x7f020006;
+        public static final int start=0x7f020007;
+        public static final int start_pressed=0x7f020008;
+        public static final int start_selector=0x7f020009;
+        public static final int status=0x7f02000a;
+        public static final int status_pressed=0x7f02000b;
+        public static final int status_selector=0x7f02000c;
+        public static final int widget_bg_normal=0x7f02000d;
+        public static final int widget_frame_portrait=0x7f02000e;
     }
     public static final class id {
         public static final int ExternalLabel=0x7f060001;
